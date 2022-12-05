5cbb7c8bd44b5b244c72ab96e90ea2cb65baf962
pabloGm1204 <pabloGm1204@gmail.com>
2022-12-05 18:51:08 +0100

    Arrays

--- funciones/arrays/array.java
+++ funciones/arrays/array.java
@@ -178,13 +178,15 @@ public class array {
      * @return el array rotado
      */
     public static int[] arrayRotadoIzquierdaInt(int x[], int n){
-        int[] a = x.clone();
-        while(n-->0){
-            int aux = a[0];
-            for(int i = 0; i<a.length; i++){
-                a[i] = a[i+1];
+        int[] a = x.clone(); // clona en a el contenido de x
+        int i;
+        int aux;
+        while (n-- > 0) {
+            aux = a[0];
+            for(i = 0; i < a.length - 1; i++) {
+                a[i] = a[i + 1];
             }
-            a[a.length-1] = aux;
+        a[a.length - 1] = aux;
         }
         return a;
     }
