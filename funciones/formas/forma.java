778ad115ff74892818b3bbe53b715ec5601cd790
pabloGm1204 <pabloGm1204@gmail.com>
2023-01-08 13:07:20 +0100

    forma

--- funciones/formas/forma.java
+++ funciones/formas/forma.java
@@ -15,4 +15,26 @@ public class forma {
             System.out.println();
         }
     }
+    /**
+     * Pinta una piramide hueca
+     * 
+     * @author Pablo GM
+     * 
+     * @param alt la altura de la piramide
+     */
+    public static void trianguloVacio(int alt){
+        for(int i = 0; i<alt; i++){
+            System.out.print("*");
+        }
+        System.out.println();
+        for(int i = 1; i<alt; i++){
+            System.out.print("*");
+            for(int j = alt-i-2; j>0; j--){
+                System.out.print(" ");
+            }
+            if(i != alt-1){
+                System.out.println("*");
+            }
+        }
+    }
 }
