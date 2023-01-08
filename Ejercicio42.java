4a238884cfa8da7a110206a01f52a72fe54d7003
pabloGm1204 <pabloGm1204@gmail.com>
2023-01-08 14:17:37 +0100

    Ejercicio42

--- Ejercicio42.java
+++ Ejercicio42.java
@@ -1,10 +1,22 @@
 import java.util.Scanner;
-import funciones.formas.forma;
 public class Ejercicio42{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Dime la altura de tu piramide: ");
         int alt = sc.nextInt();
-        forma.trianguloVacio(alt);
+        System.out.println(linea('*', alt));
+        for(int i = alt-1; i>0; i--){
+            System.out.print("*"+linea(' ', i-2));
+            if(i != 1){
+                System.out.println("*");
+            }
+        }
+    }
+    public static String linea(char caracter, int rep){
+        String resultado = "";
+        for(int i = 0; i<rep; i++){
+            resultado += caracter;
+        }
+        return resultado;
     }
 }
