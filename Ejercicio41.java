162552b88c87458a93b9f6f55c52f427c3ced4cf
pabloGm1204 <pabloGm1204@gmail.com>
2023-01-08 14:10:22 +0100

    Ejercicio41

--- Ejercicio41.java
+++ Ejercicio41.java
@@ -1,10 +1,18 @@
 import java.util.Scanner;
-import funciones.formas.forma;
 public class Ejercicio41{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Dime la altura de la piramide: ");
         int alt = sc.nextInt();
-        forma.triangulo(alt);
+        for(int i = alt; i>0; i--){
+            System.out.println(linea('*', i));
+        }
+    }
+    public static String linea(char caracter, int rep){
+        String resultado = "";
+        for(int i = 0; i<rep; i++){
+            resultado += caracter;
+        }
+        return resultado;
     }
 }
